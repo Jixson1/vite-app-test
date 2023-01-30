a252e4352796e3889a2ce435395df65ce773b39b
Jackson Buchmeyer <jackson.buchmeyer@drake.edu>
2023-01-29 18:38:37 -0600

    relocate menu icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,12 +3,14 @@ import Navigation from './Navigation';
 
 function Header() {
   return (
-    <header className="border-b p-3 flex justify-between items-center">
-      <span className="font-bold"> 
+    <header className="border-b p-3 flex items-center">
+      <span className="font-bold px-3">
         Test App
       </span>
+      <div className="border-l px-3">
+        <Navigation />
+      </div>
 
-      <Navigation />
     </header>
   );
 }
